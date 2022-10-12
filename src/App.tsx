80643895bc27fb47b77fc71517d7e02c3b5fa42d
Richard16 <farneste23@gmail.com>
2022-10-12 02:39:23 -0300

    Configurando o App.tsx e o global.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import './styles/global.css'
+
 export function App() {
 
   return (
     <>
-      <h1 className="font-bold text-violet-500 text-2xsl">Hello world</h1>
-      <button className="font-medium text-white rounded">Enviar</button>
+      <h1 className="font-bold text-violet-500 text-5xl">Hello world</h1>
+      <button className="bg-violet-500 font-medium px-4 py-2m text-white rounded hover:bg-violet-700">Enviar</button>
     </>
   )
 }
