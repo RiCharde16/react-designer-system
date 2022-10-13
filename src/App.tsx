1bac6296d61d1792a149f217b1fe9c85f3885793
Richard16 <farneste23@gmail.com>
2022-10-12 21:32:50 -0300

    Site Funcionando Se der merda da um reset ou clona isso PORFAVOR!!

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,31 @@
 import './styles/global.css'
+import {Button} from './components/button'
+import { TextInput, TextInputProps, TextInputRootprops} from './components/TextInput'
+import { Envelope } from 'phosphor-react'
+import { Checkbox } from './components/checkbox'
 
 export function App() {
 
   return (
-    <>
-      <h1 className="font-bold text-violet-500 text-5xl">Hello world</h1>
-      <button className="bg-violet-500 font-medium px-4 py-2m text-white rounded hover:bg-violet-700">Enviar</button>
-    </>
+    <main className='flex flex-col w-[500px]'>
+      <h1 className="font-bold text-2xl">Hello world</h1>
+      <button className="bg-cyan-500 font-medium px-4 py-2 w-[100px] text-white rounded hover:bg-cyan-300">Enviar</button>
+      <br/>
+      <br/>
+      {/*<Text/>*/}
+      <TextInput.Root>
+        <TextInput.Icon>
+          <Envelope/>
+        </TextInput.Icon>
+        <TextInput.Input placeholder='johdoe@example.com'
+        type='email'/>
+      </TextInput.Root>
+      <Button
+      children='Salvar  '/> 
+      <div className='flex p-2'>
+        <Checkbox/>
+        <p>Lembrar de min por 30 dias</p>
+      </div>
+    </main>
   )
 }
